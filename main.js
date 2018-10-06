4e0e497040c46dd08f90e6fe500d0cb629a3efea
yoshikimasubuchi <yoshikimasubuchi@chao-labnoMacBook-puro.local>
2018-10-06 20:40:36 +0900

    update

---
+++ main.js
@@ -0,0 +1,36 @@
+  var app = new Vue ({
+    el: '#app',
+    data: {
+      message: '2',
+      show: true
+    },
+    methods: {
+      handleClick: function (e) {
+        alert(e.target)
+      }
+    }
+  })
+
+  var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'You loaded this page on' + new Date().toLocaleString()
+    }
+  })
+  var app3 = new Vue ({
+    el: '#app-3',
+    data: {
+      seen: false
+    }
+  })
+
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' },
+      ]
+    }
+  })
