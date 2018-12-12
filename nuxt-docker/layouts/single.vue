c312e9b7c4e021f883b8a7e774c4576e310582cc
yoshikimasubuchi <yoshikimasubuchi@chao-labnoMacBook-puro.local>
2018-12-12 19:44:57 +0900

    layout追加

---
+++ nuxt-docker/layouts/single.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="">
+    <span>single layout</span>
+    <AppNavigation />
+    <hr>
+    <nuxt />
+    <nuxt-link to="/">トップに戻る</nuxt-link>
+    <hr>
+    <footer>
+      footer
+    </footer>
+  </div>
+</template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
+
+<style lang="css">
+</style>
